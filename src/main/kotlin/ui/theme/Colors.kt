a0c693893331015aeac09f4a4dc7b05a82c81705
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-11-13 09:51:08 +0700

    Add color styles

--- src/main/kotlin/ui/theme/Colors.kt
+++ src/main/kotlin/ui/theme/Colors.kt
@@ -2,13 +2,46 @@ package ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryDark = Color(0xFF5538EE)
-val PrimaryBase = Color(0xFF6B4EFF)
-
 val InkDarkest = Color(0xFF090A0A)
+val InkDarker = Color(0xFF202325)
+val InkDark = Color(0xFF303437)
 val InkBase = Color(0xFF404446)
 val InkLight = Color(0xFF6C7072)
 val InkLighter = Color(0xFF72777A)
 
 val SkyDark = Color(0xFF979C9E)
+val SkyBase = Color(0xFFCDCFD0)
 val SkyLight = Color(0xFFE3E5E5)
+val SkyLighter = Color(0xFFF2F4F5)
+val SkyLightest = Color(0xFFF7F9FA)
+val SkyWhite = Color(0xFFFFFFFF)
+
+val PrimaryDark = Color(0xFF5538EE)
+val PrimaryBase = Color(0xFF6B4EFF)
+val PrimaryLight = Color(0xFF9990FF)
+val PrimaryLighter = Color(0xFFC6C4FF)
+val PrimaryLightest = Color(0xFFE7E7FF)
+
+val GreenDarkest = Color(0xFF198155)
+val GreenBase = Color(0xFF23C16B)
+val GreenLight = Color(0xFF4CD471)
+val GreenLighter = Color(0xFF7DDE86)
+val GreenLightest = Color(0xFFECFCE5)
+
+val RedDarkest = Color(0xFFD3180C)
+val RedBase = Color(0xFFFF5247)
+val RedLight = Color(0xFFFF6D6D)
+val RedLighter = Color(0xFFFF9898)
+val RedLightest = Color(0xFFFFE5E5)
+
+val BlueDarkest = Color(0xFF0065D0)
+val BlueBase = Color(0xFF48A7F8)
+val BlueLight = Color(0xFF6EC2FB)
+val BlueLighter = Color(0xFF9BDCFD)
+val BlueLightest = Color(0xFFC9F0FF)
+
+val YellowDarkest = Color(0xFFA05E03)
+val YellowBase = Color(0xFFFFB323)
+val YellowLight = Color(0xFFFFC462)
+val YellowLighter = Color(0xFFFFD188)
+val YellowLightest = Color(0xFFFFEFD7)
