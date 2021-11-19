1972e5205dc8a2705fb40c25827da7c2ba384980
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-11-20 00:13:36 +0700

    New profile page

--- src/main/kotlin/ui/screens/Profile.kt
+++ src/main/kotlin/ui/screens/Profile.kt
@@ -1,37 +1,99 @@
 package ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import controller.UserController
-import ui.components.NavBarLarge
 import ui.components.ScreenLayout
-import ui.theme.LargeNoneMedium
-import ui.theme.RegularNoneRegular
+import ui.theme.*
+import ui.util.imagePainter
 
 @Composable
 fun Profile(onRouteChat: () -> Unit) {
+    val user = UserController.user ?: return
     ScreenLayout {
-        NavBarLarge(title = { Text(text = "Profile") })
-        Column(modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)) {
-            Text(
-                text = UserController.user?.display_name?: "",
-                style = LargeNoneMedium
+        Column {
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 24.dp, vertical = 24.dp)
+            ) {
+                Image(
+                    painter = imagePainter(user.profile_pic_url),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.padding(start = 16.dp).height(64.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = user.display_name,
+                        color = InkDarkest,
+                        style = Title3
+                    )
+                    Text(
+                        text = "${user.coin_balance} coins",
+                        color = InkDarkest,
+                        style = RegularNormalRegular
+                    )
+                }
+            }
+            RowButton(onClick = onRouteChat) {
+                Image(
+                    painter = painterResource("icons/message.svg"),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(end = 12.dp)
+                        .size(24.dp)
+                )
+                Text(text = "Chats")
+            }
+            Divider(
+                modifier = Modifier
+                    .padding(horizontal = 24.dp, vertical = 16.dp),
+                color = SkyLighter
             )
-            Text(
-                text = UserController.user?.coin_balance.toString() + " coins",
-                style = RegularNoneRegular
-            )
-            Button(onClick = { UserController.username = null }) {
+            RowButton(onClick = { UserController.username = null }) {
                 Text(text = "Logout")
             }
-            Button(onClick = onRouteChat) {
-                Text(text = "My Chats")
-            }
+        }
+    }
+}
+
+@Composable
+fun RowButton(
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .height(56.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CompositionLocalProvider(
+            LocalTextStyle provides RegularTightRegular,
+            LocalContentColor provides InkDarkest
+        ) {
+            content()
         }
     }
 }
